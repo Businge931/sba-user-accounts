8b8c62dc27e6b8728fb9f790c904fbd072dd3787
Businge931 <jimmybusinge9@gmail.com>
2025-06-17 15:53:22 +0300

    refactor: remove unused logger and validation dependencies from account service tests

--- internal/core/services/account_service_test.go
+++ internal/core/services/account_service_test.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/Businge931/sba-user-accounts/internal/adapters/secondary/validation"
 	"github.com/Businge931/sba-user-accounts/internal/core/domain"
 	cerrors "github.com/Businge931/sba-user-accounts/internal/core/errors"
 	"github.com/Businge931/sba-user-accounts/internal/core/services/mocks"
@@ -13,55 +12,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type testDeps struct {
-	userRepo  *mocks.MockUserRepository
-	authRepo  *mocks.MockAuthRepository
-	tokenSvc  *mocks.MockTokenService
-	logger    *mocks.MockLogger
-	validator *validation.Validator
-}
-
-func setUpTestDeps() *testDeps {
-	return &testDeps{
-		userRepo:  new(mocks.MockUserRepository),
-		authRepo:  new(mocks.MockAuthRepository),
-		tokenSvc:  new(mocks.MockTokenService),
-		logger:    new(mocks.MockLogger),
-		validator: validation.NewValidator(),
-	}
-}
-
-func setupLoggerExpectations(logger *mocks.MockLogger) {
-	logger.On("Debug", mock.Anything).Return()
-	logger.On("Debugf", mock.Anything, mock.Anything).Return()
-	logger.On("Info", mock.Anything).Return()
-	logger.On("Infof", mock.Anything, mock.Anything).Return()
-	logger.On("Warn", mock.Anything).Return()
-	logger.On("Warnf", mock.Anything, mock.Anything).Return()
-	logger.On("Error", mock.Anything).Return()
-	logger.On("Errorf", mock.Anything, mock.Anything).Return()
-}
-
-func newTestAccountService(deps *testDeps) *accountManagementService {
-	return &accountManagementService{
-		userRepo:  deps.userRepo,
-		authRepo:  deps.authRepo,
-		tokenSvc:  deps.tokenSvc,
-		emailSvc:  nil, // No email service for tests
-		validator: deps.validator,
-		logger:    deps.logger,
-	}
-}
-
 func TestAccountService_verifyEmail(t *testing.T) {
-	// Define test dependencies
 	type dependencies struct {
 		userRepo *mocks.MockUserRepository
 		authRepo *mocks.MockAuthRepository
 		tokenSvc *mocks.MockTokenService
 	}
 
-	// Define test arguments
 	type args struct {
 		token string
 	}
