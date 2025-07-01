f6b58708ef6ae812b074cace3770b0981140545f
Businge931 <busingebisanga99@gmail.com>
2025-07-01 04:05:16 +0300

    refactor: simplify validation to use Validator structs

--- internal/adapters/secondary/validation/validation.go
+++ internal/adapters/secondary/validation/validation.go
@@ -2,10 +2,8 @@ package validation
 
 import (
 	"regexp"
-	"strings"
 
 	"github.com/Businge931/sba-user-accounts/internal/core/domain"
-	"github.com/Businge931/sba-user-accounts/internal/core/errors"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
@@ -17,65 +15,35 @@ func NewValidator() *Validator {
 }
 
 func (v *Validator) ValidateRegisterRequest(req domain.RegisterRequest) error {
-	if err := v.ValidateEmail(req.Email); err != nil {
-		return err
-	}
-	if err := v.ValidatePassword(req.Password); err != nil {
-		return err
-	}
-	if err := v.ValidateName(req.FirstName, "first name"); err != nil {
-		return err
-	}
-	if err := v.ValidateName(req.LastName, "last name"); err != nil {
-		return err
-	}
-	return nil
+    return req.Validate()
 }
 
 func (v *Validator) ValidateLoginRequest(req domain.LoginRequest) error {
-	if err := v.ValidateEmail(req.Email); err != nil {
-		return err
-	}
-	if err := v.ValidatePassword(req.Password); err != nil {
-		return err
-	}
-	return nil
+    return req.Validate()
 }
 
 func (v *Validator) ValidateEmail(email string) error {
-	email = strings.TrimSpace(email)
-	err := validation.Validate(email,
-		validation.Required.Error("email is required"),
-		is.EmailFormat.Error("invalid email format"),
-	)
-	if err != nil {
-		return errors.NewInvalidInputError(err.Error(), nil)
-	}
-	return nil
+    return validation.Validate(
+        email,
+        validation.Required,
+        validation.Length(5, 255),
+        is.Email,
+    )
 }
 
 func (v *Validator) ValidatePassword(password string) error {
-	err := validation.Validate(password,
-		validation.Required.Error("password is required"),
-		validation.Length(8, 0).Error("password must be at least 8 characters"),
-		validation.Match(regexp.MustCompile(`[A-Z]`)).Error("password must contain at least one uppercase letter"),
-		validation.Match(regexp.MustCompile(`[a-z]`)).Error("password must contain at least one lowercase letter"),
-		validation.Match(regexp.MustCompile(`\d`)).Error("password must contain at least one digit"),
-	)
-	if err != nil {
-		return errors.NewInvalidInputError(err.Error(), nil)
-	}
-	return nil
+    return validation.Validate(
+        password,
+        validation.Required,
+        validation.Length(8, 72),
+    )
 }
 
 func (v *Validator) ValidateName(name, fieldName string) error {
-	name = strings.TrimSpace(name)
-	err := validation.Validate(name,
-		validation.Required.Error(fieldName+" is required"),
-		validation.Length(2, 0).Error(fieldName+" must be at least 2 characters"),
-	)
-	if err != nil {
-		return errors.NewInvalidInputError(err.Error(), nil)
-	}
-	return nil
+    return validation.Validate(
+        name,
+        validation.Required.Error(fieldName+" is required"),
+        validation.Length(2, 50).Error(fieldName+" must be between 2 and 50 characters"),
+        validation.Match(regexp.MustCompile(`^[\p{L} -]+$`)).Error(fieldName+" contains invalid characters"),
+    )
 }
--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,19 +1,39 @@
 package domain
 
 import (
+	"regexp"
 	"time"
+
+	"github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 type RegisterRequest struct {
-	Email     string
-	Password  string
-	FirstName string
-	LastName  string
+	Email     string `validate:"required,email"`
+	Password  string `validate:"required,min=8,max=72"`
+	FirstName string `validate:"required,min=2,max=50"`
+	LastName  string `validate:"required,min=2,max=50"`
+}
+
+func (r RegisterRequest) Validate() error {
+	return validation.ValidateStruct(&r,
+		validation.Field(&r.Email, validation.Required, validation.Length(5, 255), is.EmailFormat),
+		validation.Field(&r.Password, validation.Required, validation.Length(8, 72)),
+		validation.Field(&r.FirstName, validation.Required, validation.Length(2, 50), validation.Match(regexp.MustCompile(`^[\p{L} -]+$`))),
+		validation.Field(&r.LastName, validation.Required, validation.Length(2, 50), validation.Match(regexp.MustCompile(`^[\p{L} -]+$`))),
+	)
 }
 
 type LoginRequest struct {
-	Email    string
-	Password string
+	Email    string `validate:"required,email"`
+	Password string `validate:"required"`
+}
+
+func (l LoginRequest) Validate() error {
+	return validation.ValidateStruct(&l,
+		validation.Field(&l.Email, validation.Required, validation.Length(5, 255), is.EmailFormat),
+		validation.Field(&l.Password, validation.Required),
+	)
 }
 
 type User struct {
